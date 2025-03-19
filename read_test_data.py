ac8e8d0bd23a35845cddf5d78d0a064b5eaf606a
msenzanje <ksenzanje@gmail.com>
2025-03-19 14:35:29 -0500

    Print All Files - Define function to print text in PDF files - Print all files in Exam collection folder

--- read_test_data.py
+++ read_test_data.py
@@ -1,8 +1,19 @@
 import PyPDF2
+from pathlib import Path
 
-# The following opens an exam and prints the first page
-test1C1 =  open("Exam collection/Exam 1_Calculus 1.pdf", "rb")
-reader =  PyPDF2.PdfReader(test1C1)
+ 
+# This function returns text content within a PDF file
+def extract_text(pdf_path: str) -> str:
+    pdf = open(pdf_path, 'rb')
+    reader = PyPDF2.PdfReader(pdf)
+    n = len(reader.pages)
+    extracted = ""
+    for page in range(n):
+        extracted += reader.pages[page].extract_text()
 
-page1 = reader.pages[0]
-print(page1.extract_text())
+    return extracted
+
+# Print text in Exam collection folder
+pathlist = Path("Exam collection").glob("**/*.pdf")
+for path in pathlist:
+    print(f'{extract_text(str(path))}')
