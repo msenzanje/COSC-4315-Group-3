088e161ff6d0d055a195359fee75c0405ec2a311
msenzanje <ksenzanje@gmail.com>
2025-03-27 22:08:28 -0500

    Initialize and populate vector database
    - Set up initial vector database
    - Add files to the vector database
    - Minor fixes and improvements

--- ingest.py
+++ ingest.py
@@ -13,7 +13,7 @@ embeddings  = models.embeddings_ollama
 llm = models.model_ollama
 
 # Define cnstants
-knowledge_base = "/.knowledge_base"  # TODO: Add relevant textbook to vecor store
+knowledge_base = "./knowledge_base"  # TODO: Add relevant textbook to vecor store
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 50
 CHECK_INTERVAL =  10
@@ -30,33 +30,33 @@ vector_store = CDb(
 def  ingest_file(file_path):
     print(f'Ingesting: {file_path}')
     loader =  PyPDFLoader(file_path)
-    loaded_docs =  loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
+    loaded_docs =  loader.load() # Load and extract text from file
+    text_splitter = RecursiveCharacterTextSplitter(  # Parse file
         chunk_size = CHUNK_SIZE, chunk_overlap = CHUNK_OVERLAP,
         separators= ['\n', " ", ""]
     )
 
     documents = text_splitter.split_documents(loaded_docs)
-    uuids = [str(uuid4(documents) for _ in range(len(documents)))]
-    print(f'Adding {len(documents)} to vector space')
-    vector_store.add_documents(documents=documents, ids=uuids) # Add doc chunks in CDb
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+    print(f'Adding {len(documents)} chunks to vector space')
+    vector_store.add_documents(documents=documents, ids=uuids) # Add chunks to CDb
     print(f'Ingested: {file_path}')
 
 
 # Main Loop
-def main_loop():
+def main():
     while True:
         for filename in os.listdir(knowledge_base):
             if not filename.startswith('_'):
                 # To prevent adding same document twice
                 file_path = os.path.join(knowledge_base, filename) 
-                ingest_file(filename)
-                new_filename  = '_' + file_path
+                ingest_file(file_path)
+                new_filename  = '_' + filename
                 new_filepath = os.path.join(knowledge_base, new_filename)
                 os.rename(file_path, new_filepath)
-            time.sleep(CHECK_INTERVAL) #Check every period for new documents
+            time.sleep(CHECK_INTERVAL) # Check every period for new documents
 
 
 # Run main loop
 if __name__ == "__main__":
-    main_loop()
+    main()
--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 
 class Models:
     def __init__(self):
+        # Snowflake embedding model, BERT arch
         self.embeddings_ollama = OllamaEmbeddings(
             model = 'snowflake-arctic-embed:22m',
-            prompt = "The sky is blue"
         )
 
         self.model_ollama = ChatOllama(
